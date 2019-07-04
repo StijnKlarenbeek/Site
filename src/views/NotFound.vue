0596aaece466df614549bab3e0408f3ab541c764
Stijn <s.klarenbeek@hotmail.com>
2019-07-04 16:24:47 +0200

    First iteration on 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,8 @@
 <template>
-    <div class="404"></div>
+    <div class="page404">
+        <h1>404</h1>
+        <p>Oops, this page doesn't exist.</p>
+    </div>
 </template>
 <script>
 export default {
@@ -8,7 +11,32 @@ export default {
 </script>
 
 <style>
-    
+    .page404{
+        padding-left: 48px;
+        min-width: 100vw;
+        min-height: 100vh;
+        position: relative;
+        top:0;
+        z-index: 3;
+        padding-top: 112px;
+    }
+    h1{
+        font-size: 365px;
+        line-height: 365px;
+        font-weight: 900;
+        margin-top: 0px;
+        margin-bottom: 0px;
+        margin-left: -12px;
+        color: #030005;
+        text-transform: uppercase;
+        text-shadow: -1px -1px 0px #68FFD1, 1px 1px 0px #F5BA78;
+        letter-spacing: -35px;
+    }
+    p{
+        color: #fff;
+        font-weight: 700;
+        font-size: 55px;
+    }
 </style>
 
 
